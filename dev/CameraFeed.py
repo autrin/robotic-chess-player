1dbd3f829d6bf99830083d2456a39c732db87db3
jshim <jshim@iastate.edu>
2025-03-02 15:43:43 -0600

    successfully partitioned the axes

--- dev/CameraFeed.py
+++ dev/CameraFeed.py
@@ -39,6 +39,9 @@ class CameraFeed:
 
     #returns real number. Do not convert the output of this function into an int. Only convert the final coordinate
     def calculateSlope(self, point0, point1):
+        print(f"p0x = {point0[0]}, p1x = {point1[0]}")
+        if point0[0] - point1[0] == 0:
+            return 0
         return (point0[1] - point1[1])/(point0[0] - point1[0])
 
     #logic for setting the boundaries of both the outer chess board and each square within the board
@@ -69,7 +72,71 @@ class CameraFeed:
         TL2 = [int(TL2[0]),int(TL2[1])]
         BL1 = [int(BL1[0]),int(BL1[1])]
         BR0 = [int(BR0[0]),int(BR0[1])]
-        return TL2, BL1, BR0
+        return [TL2, BL1, BR0]
+    
+    def getAxesIntervalDots(self,TL,BL,BR):
+        print(f"TL = {TL}")
+        width = self.calculateEuclidianDist(BL,BR)
+        height = self.calculateEuclidianDist(TL,BL)
+
+        widthInterval = width/8
+        heightInterval = height/8
+        
+        hSlope = self.calculateSlope(BL,BR)
+        vSlope = self.calculateSlope(TL,BL)
+
+        hMagInverse = (math.sqrt(1+hSlope**2))
+        vMagInverse = (math.sqrt(1+vSlope**2))
+        
+        horizontalUnitVect = [1/hMagInverse, hSlope/hMagInverse]
+        verticalUnitVect = [1/vMagInverse,vSlope/vMagInverse]
+        
+        hdirx = 1
+        hdiry = 1
+
+        vdirx = 1
+        vdiry = 1
+
+        #we start plotting the dots from BL, so the direction is important
+        if TL[0] < BL[0]:
+            vdirx = -1
+        if TL[1] < BL[1]:
+            vdiry = -1
+
+        if BR[0] < BL[0]:
+            hdirx = -1
+        if BR[1] < BL[1]:
+            vdiry = -1
+
+        hPoints = []
+        vPoints = []
+        #up to 9 because we don't have the other side
+        for dis in range(1,9):
+            h = []
+            v = []
+            
+            h.append(BL[0] +hdirx*dis*widthInterval*horizontalUnitVect[0])
+            h.append(BL[1] +hdiry*dis*widthInterval*horizontalUnitVect[1])
+            hPoints.append(h)
+            
+            v.append(BL[0] + vdirx*dis*heightInterval*verticalUnitVect[0])
+            v.append(BL[1] + vdirx*dis*heightInterval*verticalUnitVect[1])
+            vPoints.append(v)
+        
+        return hPoints, vPoints
+    
+    def plotDotsOnAxes(self,frame,hPoints,vPoints):
+        for hp in hPoints:
+            cv2.circle(frame,(int(hp[0]),int(hp[1])),3,(0,0,255),-1)
+
+        for vp in vPoints:
+            cv2.circle(frame,(int(vp[0]),int(vp[1])),3,(0,0,255),-1)
+
+
+
+
+
+
 
     def drawLine(self, frame, p0,p1):
         cv2.line(frame, p0, p1, color=(0, 255, 0), thickness=1)
@@ -126,6 +193,8 @@ class CameraFeed:
             
             if cbc:
                 corners = self.get_chessboard_boundaries(cbc)
+                hpoints,vpoints = self.getAxesIntervalDots(corners[0],corners[1],corners[2])
+                self.plotDotsOnAxes(frame,hpoints,vpoints)
                 self.drawLine(frame,corners[0],corners[1])
                 self.drawLine(frame,corners[1],corners[2])
                 #self.drawAprilTagCorner(frame,detections,1)
