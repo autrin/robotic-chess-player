3b175a1feaa5bdf2f1f7bf2ea42f2ef71a77f5ae
ahakimi <ahakimi@iastate.edu>
2025-03-11 17:08:07 -0500

    added imports

--- dev/talker.py
+++ dev/talker.py
@@ -1,1 +1,4 @@
 #!/usr/bin/env python
+import rospy # for writing a ROS node
+from std_msgs.msg import string # so we can reuse the std_msgs/String message type (a simple string container) for publishing
+
