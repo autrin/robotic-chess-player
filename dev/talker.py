fefbf9759f21012973f09810c11c1c572d0927d9
ahakimi <ahakimi@iastate.edu>
2025-03-11 17:14:06 -0500

    publish to the topic and initialize the node

--- dev/talker.py
+++ dev/talker.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 import rospy # for writing a ROS node
-from std_msgs.msg import string # so we can reuse the std_msgs/String message type (a simple string container) for publishing
+from std_msgs.msg import String # so we can reuse the std_msgs/String message type (a simple string container) for publishing
 
+def talker():
+  pub = rospy.Publisher('chatter', String, queue_size=10) # the node publishes to the chatter topic using the message type String. 
+  rospy.init_node('talker', anonymous=True) # name of the node is talker
+                                            # anonymous makes the name unique by adding random numbers to the end of the name
+  
