dd31a5a8cc32fb17b3108759d51354b87f0a3ca5
jshim <jshim@iastate.edu>
2025-03-26 22:49:04 -0500

    testing shows that tags can get confused. need to get best combos such that the detector does not get confused

--- dev/GamePlay.py
+++ dev/GamePlay.py
@@ -9,7 +9,7 @@ class GamePlayClass:
         self.chessEngine = ChessEngineClass()
         #assume robot will always play white by default
         
-        #tagID: [pieceType,cellPos]
+        #tagID: [pieceType,cellPos] pieceType == "x" if captured
         self.tagIDWhitePieces = { 
             0 : ['P',8],
             1 : ['P',9],
@@ -48,9 +48,28 @@ class GamePlayClass:
         }
         self.tagIDToMyPieces = None
         self.tagIDToOppPieces = None
-
-
         #self.homographyCorners = None
+    
+    def cellPosToChessCellPos(self,num):
+        rank = num//8
+        rank = chr(ord('a') + rank)
+        col = str(num%8)
+        return rank+col
+
+    #need castling, en-passant, promotion
+    #focus on the base cases for now
+    def getOppMoveFromVisual(self,oppPieces):
+        for op in oppPieces:
+            newPos = self.camera.getCellPosofPiece(int(op.center[0]),int(op.center[1]))
+            #position has changed, assume only one piece gets to move
+            if self.tagIDToOppPieces[op.tag_id][1] != newPos:
+                oldCellString = self.cellPosToChessCellPos(self.tagIDToOppPieces[op.tag_id][1])
+                newCellString = self.cellPosToChessCellPos(newPos)
+                return oldCellString+newCellString
+
+
+
+
 
     def calibrate(self):
         self.camera.openCamera()
@@ -64,8 +83,10 @@ class GamePlayClass:
             
             if detections:
                 fp = self.camera.get_chessboard_boundaries(detections)
+                
                 if fp:
                     warpedFrame = self.camera.getHomoGraphicAppliedImage(grayFrame,fp)
+                    #warpedFrame = cv2.resize(warpedFrame, (0, 0), fx = 0.1, fy = 0.1)
                     if warpedFrame is not None:
                         detections = self.camera.aprilDetector.detect(img=warpedFrame)
                         warpedFrame = cv2.cvtColor(warpedFrame,cv2.COLOR_GRAY2BGR)
@@ -78,11 +99,7 @@ class GamePlayClass:
                 break
         self.camera.destroyCameraFeed()
 
-
-    def play(self):
-        
-        self.calibrate()
-        
+    def determine_side(self):
         #assume from this point on, the board has been calibrated
         while True:
             whoGoesFirst = input("who goes first? (ai/human)")
@@ -100,30 +117,38 @@ class GamePlayClass:
             else:
                 print("invalid input, try again\n")
 
-        #continuous calibration???
+
+    def play(self):
+        
+        
+        self.calibrate()
+        self.determine_side()
+
         self.camera.openCamera()
         while True:
             ret,frame = self.camera.cam.read()
-            
             grayFrame = self.camera.convertToTagDetectableImage(frame)
-            #pieceDetections = self.camera.myChessPieceDetector.detect(img=grayFrame)
             cornerDetections = self.camera.aprilDetector.detect(img=grayFrame)
             
+            #Every programmer's dream: just nest everything
             if cornerDetections:
                 fp = self.camera.get_chessboard_boundaries(cornerDetections)
                 if fp:
                     warpedFrame = self.camera.getHomoGraphicAppliedImage(grayFrame,fp)
                     if warpedFrame is not None:
-                        detections = self.camera.aprilDetector.detect(img=warpedFrame)
-                        warpedFrame = cv2.cvtColor(warpedFrame,cv2.COLOR_GRAY2BGR)
-                        self.camera.drawBordersandDots(warpedFrame,detections)
-                        cv2.imshow(f"warped",warpedFrame)
-                        
-
-
-            cv2.imshow(f"FEED Cam-ID = {self.camera.camID}",frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                        myPieceDetections = self.camera.myChessPieceDetector.detect(img=warpedFrame)
+                        oppPieceDetections = self.camera.oppChessPieceDetector.detect(img=warpedFrame)
+                        if myPieceDetections and oppPieceDetections:
+                                
+                                cornerDetections = self.camera.aprilDetector.detect(img=warpedFrame)
+                                warpedFrame = cv2.cvtColor(warpedFrame,cv2.COLOR_GRAY2BGR)
+                                self.camera.drawBordersandDots(warpedFrame,cornerDetections)
+                                self.camera.drawPieces(warpedFrame,oppPieceDetections,myPieceDetections,self)
+                                cv2.imshow(f"warped",warpedFrame)
+                                
+                cv2.imshow(f"FEED Cam-ID = {self.camera.camID}",frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
         self.camera.destroyCameraFeed()
 
 if __name__ == "__main__":
