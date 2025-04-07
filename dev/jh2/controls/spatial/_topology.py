1e7b0045b01f4ee8131be4d297e5ece494fade17
Thanh <thanhm@iastate.edu>
2025-04-07 16:13:03 -0500

    new package for end effector hande gripper

--- dev/jh2/controls/spatial/_topology.py
+++ dev/jh2/controls/spatial/_topology.py
@@ -1,3 +1,23 @@
-class WaypointTopology:
-    def __init__(self):
-        pass
+from typing import List, Dict
+import numpy as np
+
+from jh2.controls.spatial._waypoint import Waypoint
+
+
+class Topology:
+    def __init__(self, waypoints: List[Waypoint], connectivity: Dict[int, int]):
+        self.waypoints: List[Waypoint] = waypoints
+        self.adjacency: Dict[int, int] = connectivity
+
+
+    @staticmethod
+    def standard_board(
+        piece_elevation: float,
+        travel_elevation: float,
+    ) -> 'Topology':
+        """
+        TODO
+        generates a
+
+        """
+        # s =
