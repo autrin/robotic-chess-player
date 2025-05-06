0652c078ab20ac4b41912d1998af6ae2d023d002
Thanh <thanhm@iastate.edu>
2025-05-05 19:49:56 -0500

    deprecate old code

---
+++ src/jh1/utils/mathematics/__init__.py
@@ -0,0 +1,6 @@
+from _affine import (
+    rot_2d_mat3, scale_2d_mat3, shear_2d_mat3, transl_2d_mat3,
+    shift_center_mat3, unshift_center_mat3, compose_mat3,
+    rot_roll_mat4, rot_pitch_mat4, rot_azimuth_mat4,
+    transl_3d_mat4, se3_transform_mat4
+)
